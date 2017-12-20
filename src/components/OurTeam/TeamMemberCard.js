b7a1803a792473bba60519af1529636f159636b9
Calvin Rafferty <calvinhobbes352@gmail.com>
2017-12-20 08:49:56 -0800

    snow

--- src/components/OurTeam/TeamMemberCard.js
+++ src/components/OurTeam/TeamMemberCard.js
@@ -4,10 +4,21 @@ class TeamMemberCard extends Component {
   render() {
     return (
       <Container>
-        <Card1 />
-        <Card2 />
-        <Card3 />
-        <Card4 />
+        <Row1>
+          <Card1 />
+          <Card2 />
+          <Card3 />
+          <Card4 />
+        </Row1>
+        <Column>
+          <Text>Our Philosophy</Text>
+          <Phil>
+            It’s about making placeholder text great again. That’s what people
+            want, they want placeholder text to be great again. An 'extremely
+            credible source' has called my office and told me that Lorem Ipsum's
+            birth certificate is a fraud.
+          </Phil>
+        </Column>
       </Container>
     );
   }
@@ -15,40 +26,71 @@ class TeamMemberCard extends Component {
 
 export default TeamMemberCard;
 
+const Text = glamorous.h1({
+  padding: "30px"
+});
+
+const Phil = glamorous.div({
+  textAlign: "center"
+});
+
 const Container = glamorous.div({
   flex: 1,
-  flexDirection: "row",
+  flexDirection: "column",
   display: "flex",
-  flexFlow: "row wrap",
-  padding: 5,
-  justifyContent: "space-between"
+  width: 1440,
+  height: 614
+});
+
+const Row1 = glamorous.div({
+  flex: 1,
+  flexDirection: "row",
+  justifyContent: "space-between",
+  margin: 5,
+  display: "flex"
+});
+
+const Column = glamorous.div({
+  flex: 1,
+  flexDirection: "column",
+  display: "flex"
 });
 
 const Card1 = glamorous.div({
-  backgroundImage: 'url("images/projects/project-1.png")',
+  backgroundSize: "cover",
+  backgroundImage: 'url("images/team-photos/Isaiah.jpg")',
   display: "flex",
   flexDirection: "column",
-  width: 465,
-  height: 470
+  width: 242,
+  height: 346
 });
+
 const Card2 = glamorous.div({
-  backgroundImage: 'url("images/projects/project-2.png")',
+  backgroundSize: "cover",
+
+  backgroundImage: 'url("images/team-photos/Chris 0120.jpg")',
   display: "flex",
   flexDirection: "column",
-  width: 465,
-  height: 470
+  width: 242,
+  height: 346
 });
+
 const Card3 = glamorous.div({
-  backgroundImage: 'url("images/projects/project-3.png")',
+  backgroundSize: "cover",
+
+  backgroundImage: 'url("images/team-photos/Gabe 0110.jpg")',
   display: "flex",
   flexDirection: "column",
-  width: 465,
-  height: 470
+  width: 242,
+  height: 346
 });
+
 const Card4 = glamorous.div({
-  backgroundImage: 'url("images/projects/project-4.png")',
+  backgroundSize: "cover",
+
+  backgroundImage: 'url("images/team-photos/Calvin.jpg")',
   display: "flex",
   flexDirection: "column",
-  width: 465,
-  height: 470
+  width: 242,
+  height: 346
 });
