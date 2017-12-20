2140553a57894f73befc3d40885ef544ffeafea8
Gabriel Martin <gabriel.martin91@outlook.com>
2017-12-20 09:25:48 -0800

    Check in

--- src/components/OurTeam/TeamMemberCard.js
+++ src/components/OurTeam/TeamMemberCard.js
@@ -1,61 +1,29 @@
-// import React, { Component } from "react";
-
-// class TeamMemberCard extends Component {
-//   render() {
-//     return (
-//       <Container>
-//         <Card1>
-//           <ImgWell>
-//             <Img src={"images/team/Isaiah.jpg"} />
-//           </ImgWell>
-//           <CardFooter>
-//             <Name>Isaiah Grey</Name>
-//             <Role>Co-Founder | Lead Developer</Role>
-//           </CardFooter>
-//         </Card1>
-//         <Card2>
-//           <ImgWell>
-//             <Img src={"images/team/Chris.jpg"} />
-//           </ImgWell>
-//           <CardFooter>
-//             <Name>Isaiah Grey</Name>
-//             <Role>Co-Founder | Lead Developer</Role>
-//           </CardFooter>
-//         </Card2>
-//         <Card3>
-//           <ImgWell>
-//             <Img src={"images/team/Calvin.jpg"} />
-//           </ImgWell>
-//           <CardFooter>
-//             <Name>Isaiah Grey</Name>
-//             <Role>Co-Founder | Lead Developer</Role>
-//           </CardFooter>
-//         </Card3>
-//         <Card4>
-//           <ImgWell>
-//             <Img src={"images/team/Gabe.jpg"} />
-//           </ImgWell>
-//           <CardFooter>
-//             <Name>Isaiah Grey</Name>
-//             <Role>Co-Founder | Lead Developer</Role>
-//           </CardFooter>
-//         </Card4>
-//       </Container>
-//     );
-//   }
-// }
-
-// export default TeamMemberCard;
-
 import React from "react";
 
 import glamorous from "glamorous";
 
 const TeamMemberCard = props => {
-  return <div>card</div>;
+  return (
+    <Card>
+      <ImgWell>
+        <Img src={props.image} />
+      </ImgWell>
+      <CardFooter>
+        <Name>{props.name}</Name>
+        <Position>{props.position}</Position>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default TeamMemberCard;
+
+const Card = glamorous.div({
+  width: 242,
+  display: "flex",
+  flexDirection: "column",
+  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.4)"
+});
 
 const ImgWell = glamorous.div({
   height: 300,
@@ -79,40 +47,6 @@ const Name = glamorous.div({
   margin: "1px 3px"
 });
 
-const Role = glamorous.div({
+const Position = glamorous.div({
   margin: "1px 3px"
-});
-
-const Container = glamorous.div({
-  flex: 1,
-  padding: 5,
-  display: "flex",
-  flexDirection: "row",
-  flexFlow: "row wrap",
-  justifyContent: "space-around"
-});
-
-const Card1 = glamorous.div({
-  width: 242,
-  display: "flex",
-  flexDirection: "column",
-  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.4)"
-});
-const Card2 = glamorous.div({
-  width: 242,
-  display: "flex",
-  flexDirection: "column",
-  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.4)"
-});
-const Card3 = glamorous.div({
-  width: 242,
-  display: "flex",
-  flexDirection: "column",
-  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.4)"
-});
-const Card4 = glamorous.div({
-  width: 242,
-  display: "flex",
-  flexDirection: "column",
-  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.4)"
 });
--- src/components/OurTeam/index.js
+++ src/components/OurTeam/index.js
@@ -1,17 +1,58 @@
+import React, { Component } from "react";
+
+import glamorous from "glamorous";
+
 import OurTeamHeader from "./OurTeamHeader";
 import TeamMemberCard from "./TeamMemberCard";
-
-import React, { Component } from "react";
 
 class OurTeam extends Component {
   render() {
     return (
-      <div>
-        <OurTeamHeader />
-        <TeamMemberCard />
-      </div>
+      <Container>
+        <HeaderRow>
+          <OurTeamHeader />
+        </HeaderRow>
+        <CardRow>
+          <TeamMemberCard
+            name={"Isaiah Grey"}
+            image={"images/team/Isaiah.jpg"}
+            position={"Co-Founder | Lead Developer"}
+          />
+          <TeamMemberCard
+            name={"Chris Nicolls"}
+            image={"images/team/Chris.jpg"}
+            position={"Co-Founder | Project Manager"}
+          />
+          <TeamMemberCard
+            position={"Developer"}
+            name={"Calvin Rafferty"}
+            image={"images/team/Calvin.jpg"}
+          />
+          <TeamMemberCard
+            position={"Developer"}
+            name={"Gabriel Martin"}
+            image={"images/team/Gabe.jpg"}
+          />
+        </CardRow>
+      </Container>
     );
   }
 }
 
 export default OurTeam;
+
+const Container = glamorous.div({
+  display: "flex",
+  flexDirection: "column"
+});
+
+const HeaderRow = glamorous.div({
+  flex: 1
+});
+
+const CardRow = glamorous.div({
+  display: "flex",
+  margin: "50px 0px",
+  flexFlow: "row wrap",
+  justifyContent: "space-around"
+});
